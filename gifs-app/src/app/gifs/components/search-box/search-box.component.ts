fd183724e268b0d7dc039fa65f05483db2cddb95
jhon <jhon.bocanegra26@gmail.com>
2024-04-19 20:59:26 -0500

    add: gifs service

--- gifs-app/src/app/gifs/components/search-box/search-box.component.ts
+++ gifs-app/src/app/gifs/components/search-box/search-box.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild, inject } from '@angular/core';
+import { GifsService } from '../../services/gifs.service';
 
 @Component({
   selector: 'gifs-search-box',
@@ -6,5 +7,14 @@ import { Component } from '@angular/core';
   styleUrl: './search-box.component.css'
 })
 export class SearchBoxComponent {
+  private gifsService = inject(GifsService)
+  @ViewChild('txtTagInput')
+  public tagInput!: ElementRef<HTMLInputElement>
 
+  searchTag() {
+    const newTag = this.tagInput.nativeElement.value
+    this.gifsService.searchTag(newTag)
+
+    this.tagInput.nativeElement.value = ''
+  }
 }
