f470e289e70c99dd313105a69f205893e04bbbe8
jhon <jhon.bocanegra26@gmail.com>
2024-09-30 20:39:01 -0500

    feat: Add SharedModule to app.module.ts and auth.module.ts
    chore: Add AuthRoutingModule to auth.module.ts
    
    feat: Add layout, login, and register components to auth.module.ts
    
    feat: Add hero, layout, list, new, and search components to heroes.module.ts
    
    feat: Add layout component to heroes/pages/layout folder
    
    feat: Add hero component to heroes/pages/hero folder
    
    feat: Add list component to heroes/pages/list folder
    
    feat: Add new component to heroes/pages/new folder
    
    feat: Add search component to heroes/pages/search folder
    
    chore: Update MaterialModule exports in material.module.ts
    
    feat: Add error404 component to shared.module.ts

--- heroes-app/src/app/heroes/heroes.module.ts
+++ heroes-app/src/app/heroes/heroes.module.ts
@@ -2,10 +2,21 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { HeroesRoutingModule } from './heroes-routing.module';
+import { HeroComponent } from './pages/hero/hero.component';
+import { LayoutComponent } from './pages/layout/layout.component';
+import { ListComponent } from './pages/list/list.component';
+import { NewComponent } from './pages/new/new.component';
+import { SearchComponent } from './pages/search/search.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    HeroComponent,
+    LayoutComponent,
+    ListComponent,
+    NewComponent,
+    SearchComponent
+  ],
   imports: [
     CommonModule,
     HeroesRoutingModule
--- heroes-app/src/app/shared/shared.module.ts
+++ heroes-app/src/app/shared/shared.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-
+import { Error404Component } from './pages/error404/error404.component';
 
 
 @NgModule({
-  declarations: [],
-  imports: [
-    CommonModule
+  declarations: [
+    Error404Component
+  ],
+  exports: [
+    Error404Component
   ]
 })
 export class SharedModule { }
